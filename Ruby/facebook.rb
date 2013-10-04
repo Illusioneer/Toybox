b0195d2b4b05da8c5697567a4bf5a4f6909e86ee
Illusioneer <bryano@linux-4jis.site>
2013-10-04 14:46:48 -0400

    Added test file to new branch only.

--- Ruby/facebook.rb
+++ Ruby/facebook.rb
@@ -1,13 +1,13 @@
 require 'rss'
 
 response = RSS::Parser.parse('https://www.facebook.com/feeds/notifications.php?id=598149827&viewer=598149827&key=AWhyICIe_v0yXiyX&format=rss20', false)
-checktime = Time.now - 1900
+checktime = Time.now - 180
 
 response.items.each do |item|
   if item.pubDate > checktime
     p item.pubDate
     p checktime
-    system "kdialog --title '#{item.title}' --icon '/home/bryano/Pictures/fb_logo.jpg' --passivepopup '#{item.description}'"
+    %x(kdialog --title '#{item.title}' --icon '/home/bryano/Pictures/fb_logo.jpg' --passivepopup '#{item.description.gsub(/\n/," ")}')
     p item.title
   end
 end
